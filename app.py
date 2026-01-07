e65b0058e77c7fd34b38c3c02e3ec7642a74e83f
KongNhel <kabkong94@gmail.com>
2026-01-07 13:29:53 +0700

    jui

--- app.py
+++ app.py
@@ -73,5 +73,7 @@ def remove_background_via_api():
     except requests.exceptions.RequestException as e:
         return jsonify({'success': False, 'error': f"Network Error: Could not connect to API. {e}"}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
